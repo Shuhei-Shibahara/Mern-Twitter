3fdf7863c0358f7368a1e98406e7810dd2dcb814
Shuhei Shibahara <Shoehey06@gmail.com>
2022-11-13 10:54:23 -0800

    create csrf token

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,46 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const csurf = require('csurf')
 
-var usersRouter = require('./routes/api/users');
-var tweetsRouter = require('./routes/api/tweets')
+// ADD THESE TWO LINES
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
 
-var app = express();
+const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api.csrf')
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/users', usersRouter); // update the path
-app.use('/api/tweets', tweetsRouter)
+// ...
+
+// ADD THIS SECURITY MIDDLEWARE
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, the Express 
+  // server will serve the React files statically.)
+  app.use(cors());
+}
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+// Attach Express routers
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
